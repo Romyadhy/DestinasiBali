46cbac9f805945b2b3404c5c9f4b1ab9dffe4b6e
Romyadhy <romyadhy@student.undiksha.ac.id>
2025-01-05 21:47:48 +0800

    up first

---
+++ app/Filament/Resources/DestinasiResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DestinasiResource\Pages;
+use App\Filament\Resources\DestinasiResource\RelationManagers;
+use App\Models\Destinasi; 
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DestinasiResource extends Resource
+{
+    protected static ?string $model = Destinasi::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-list-bullet';
+    protected static ?string $navigationLabel = 'Semua Destinasi';
+    protected static ?string $navigationGroup = 'Kelola Data';
+    protected static ?string $slug = 'Destinasi';
+    public static ?string $label = 'Data Destinasi';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('nama destinasi')
+                    ->label('Nama Destinasi')
+                    ->required()
+                    ->placeholder('Masukkan nama destinasi'),
+                TextInput::make('detail')
+                    ->label('Detail')
+                    ->required()
+                    ->placeholder('Masukkan detail destinasi'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nama destinasi')
+                    ->label('Nama Destinasi')
+                    ->searchable()
+                    ->copyable(),
+                TextColumn::make('detail')
+                    ->label('Detail')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDestinasis::route('/'),
+            'create' => Pages\CreateDestinasi::route('/create'),
+            'edit' => Pages\EditDestinasi::route('/{record}/edit'),
+        ];
+    }
+}
